aa620ca70c6badfd04dd596752e5b6f6ab2962af
RuanDevz <Ruanbatista1509@outlook.com>
2024-07-15 20:31:31 -0300

    feat:create woman

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <Navbar/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
